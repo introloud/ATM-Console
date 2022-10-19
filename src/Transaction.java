7e1773a74d7a6730c907c17ef236c4a4cb465d0e
introloud <s_malla156680@fanshaweonline.ca>
2022-10-18 21:55:31 -0400

    added transaction constructors

--- src/Transaction.java
+++ src/Transaction.java
@@ -13,5 +13,31 @@ public class Transaction {
 	
 	// The account in which the transaction was performed
 	private Account inAccount;
+	
+	/**
+	 * Create a new transaction
+	 * @param amount		the amount transacted
+	 * @param inAccount		the account the transaction belongs to
+	 */
+	public Transaction(double amount, Account inAccount) {
+		
+		this.amount = amount;
+		this.inAccount = inAccount;
+		this.timestamp = new Date();
+		this.memo = "";
+	}
+	/**
+	 * Create a new transaction
+	 * @param amount		the amount transacted
+	 * @param memo			the memo for the transaction
+	 * @param inAccount		the account the transaction belongs to
+	 */
+	public Transaction(double amount, String memo, Account inAccount) {
+		
+		//call the two-arg constructor
+		this(amount, inAccount);
+		
+		this.memo = memo;
+	}
 
 }
